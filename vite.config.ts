d5d44ad72a2046665c5493f7b0497da2d4109a52
Koh Yi Tong <tonglenovo7@gmail.com>
2026-09-17 05:02:40 +0800

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: '/my-personal-website/',
+  base: '/TCGSite/',
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
